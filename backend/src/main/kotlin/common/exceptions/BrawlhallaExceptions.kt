0fb4ba2fdfa4000b52632c6174706f5e83ac513f
clemlery <lerouleyclement@gmail.com>
2026-04-25 23:05:07 +0200

    Enhance Brawlhalla API error handling with custom exceptions and implement rate limit handling in BrawlhallaDao

--- backend/src/main/kotlin/common/exceptions/BrawlhallaExceptions.kt
+++ backend/src/main/kotlin/common/exceptions/BrawlhallaExceptions.kt
@@ -1,15 +1,46 @@
 package com.brawlpulse.api.common.exceptions
 
-sealed class BhApiException : Exception()
+sealed class BhApiException(
+    message: String? = null,
+    cause: Throwable? = null
+) : Exception(message, cause)
 
-class PlayerNotFoundException : BhApiException()
+class IllegalStateException(
+    message: String? = null,
+    cause: Throwable? = null
+) : BhApiException(message, cause)
 
-class InvalidApiKeyException : BhApiException()
+class InvalidApiKeyException(
+    message: String? = null,
+    cause: Throwable? = null
+) : BhApiException(message, cause)
 
-class RateLimitExceededAfterRetryException : BhApiException()
+class NotFoundException(
+    message: String? = null,
+    cause: Throwable? = null
+) : BhApiException(message, cause)
 
-class ServerErrorException : BhApiException()
+class BadRequestException(
+    message: String? = null,
+    cause: Throwable? = null
+) : BhApiException(message, cause)
 
-class NetworkErrorException : BhApiException()
-// Serializable
-class DeserializationException : BhApiException()
+class RateLimitExceededException(
+    message: String? = null,
+    cause: Throwable? = null
+) : BhApiException(message, cause)
+
+class ServerErrorException(
+    message: String? = null,
+    cause: Throwable? = null
+) : BhApiException(message, cause)
+
+class UncoveredException(
+    message: String? = null,
+    cause: Throwable? = null
+) : BhApiException(message, cause)
+
+class DeserializationException(
+    message: String? = null,
+    cause: Throwable? = null
+) : BhApiException(message, cause)
--- backend/src/main/kotlin/infrastructure/brawlhalla/BrawlhallaDao.kt
+++ backend/src/main/kotlin/infrastructure/brawlhalla/BrawlhallaDao.kt
@@ -1,18 +1,50 @@
 package com.brawlpulse.api.infrastructure.brawlhalla
 
+import com.brawlpulse.api.common.exceptions.*
 import com.brawlpulse.api.infrastructure.brawlhalla.models.PlayerStatsGlobalResponse
 import com.brawlpulse.api.infrastructure.brawlhalla.models.PlayerStatsRankedResponse
 import com.brawlpulse.api.infrastructure.brawlhalla.models.SearchPlayerResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpResponseValidator
+import io.ktor.client.plugins.ResponseException
 import io.ktor.client.request.get
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.*
 
 class BrawlhallaDao {
 
     private val client = HttpClient(CIO) {
+        install(HttpRequestRetry) {
+            maxRetries = 3
+            retryIf { request, response ->
+                (response.status == HttpStatusCode.TooManyRequests)
+            }
+            retryOnServerErrors(maxRetries = 3)
+            exponentialDelay()
+        }
+        HttpResponseValidator {
+            handleResponseExceptionWithRequest { exception, request ->
+                val clientException = exception as? ResponseException ?: return@handleResponseExceptionWithRequest
+                val exceptionResponse = clientException.response
+                val exceptionResponseText = exceptionResponse.bodyAsText()
+                when (exceptionResponse.status) {
+                    HttpStatusCode.Unauthorized -> throw IllegalStateException(exceptionResponseText)
+                    HttpStatusCode.Forbidden -> throw InvalidApiKeyException(exceptionResponseText)
+                    HttpStatusCode.NotFound -> {
+                        if (exceptionResponseText.startsWith("Not Found")) throw NotFoundException(exceptionResponseText)
+                        else throw BadRequestException(exceptionResponseText)
+                    }
+                    HttpStatusCode.TooManyRequests -> throw RateLimitExceededException(exceptionResponseText)
+                    HttpStatusCode.ServiceUnavailable -> throw ServerErrorException(exceptionResponseText)
+                    else -> throw UncoveredException(exceptionResponseText)
+                }
+            }
+        }
         install(ContentNegotiation) {
             json()
         }
