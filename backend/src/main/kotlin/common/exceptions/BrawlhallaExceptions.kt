59f1bd3165be2efedd30b128028c55090c077820
clemlery <lerouleyclement@gmail.com>
2026-04-25 18:18:37 +0200

    Add custom exception classes for Brawlhalla API error handling

--- backend/src/main/kotlin/common/exceptions/BrawlhallaExceptions.kt
+++ backend/src/main/kotlin/common/exceptions/BrawlhallaExceptions.kt
@@ -1,2 +1,15 @@
 package com.brawlpulse.api.common.exceptions
 
+sealed class BhApiException : Exception()
+
+class PlayerNotFoundException : BhApiException()
+
+class InvalidApiKeyException : BhApiException()
+
+class RateLimitExceededAfterRetryException : BhApiException()
+
+class ServerErrorException : BhApiException()
+
+class NetworkErrorException : BhApiException()
+// Serializable
+class DeserializationException : BhApiException()
